da99d1cf3165d73a83e96dc369fa19f11bc7d3ae
Patrick Faramaz <faramazpat@gmail.com>
2020-11-16 06:58:25 +0100

    set not found

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,40 +1,10 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import DefaultErrorPage from "next/error";
 import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function Post({ entry }) {
-  //   const route = useRouter();
-  //   const [_entry, setEntry] = useState(entry);
-  //   useEffect(async () => {
-  //     const { query } = route;
-  //     const { token, slug } = query;
-  //     if (
-  //       !_entry &&
-  //       token &&
-  //       (query["x-craft-live-preview"] || query["x-craft-preview"])
-  //     ) {
-  //       // if craft preview send token
-  //       const headers = {
-  //         "x-Craft-Token": token ? `${token}` : "",
-  //       };
-  //       const { data } = await axios.post(process.env.API_URL, {
-  //         query: `
-  //             query BlogPost {
-  //             entry(slug: "${slug}", status: ["live", "disabled"]) {
-  //                 ...on blog_blog_Entry {
-  //                     title
-  //                     body
-  //                     status
-  //                 }
-  //             }
-  //         }
-  //     `,
-  //       });
-  //       const { entry } = data.data.entry;
-  //       setEntry(entry);
-  //     }
-  //   }, [route]);
   return (
     <>
       {entry ? (
@@ -89,9 +59,15 @@ export async function getStaticProps({ params, preview, previewData }) {
   const { status } = data.data.entry;
   const display =
     status === "live" ? true : status === "disabled" && preview ? true : false;
+  if (display === false) {
+    return {
+      notFound: true,
+      props: {},
+    };
+  }
   return {
     props: {
-      entry: display ? data.data.entry : {},
+      entry: data.data.entry,
     },
   };
 }
