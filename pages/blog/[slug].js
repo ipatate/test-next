45d432acb686c36a6944c9fc44dc91a0abd76a89
Patrick Faramaz <faramazpat@gmail.com>
2020-11-16 06:31:51 +0100

    modify blog page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,49 +4,49 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function Post({ entry }) {
-  const route = useRouter();
-  const [_entry, setEntry] = useState(entry);
-  useEffect(async () => {
-    const { query } = route;
-    const { token, slug } = query;
-    if (
-      !_entry &&
-      token &&
-      (query["x-craft-live-preview"] || query["x-craft-preview"])
-    ) {
-      // if craft preview send token
-      const headers = {
-        "x-Craft-Token": token ? `${token}` : "",
-      };
-      const { data } = await axios.post(process.env.API_URL, {
-        query: `
-            query BlogPost {
-            entry(slug: "${slug}", status: ["live", "disabled"]) {
-                ...on blog_blog_Entry {
-                    title
-                    body
-                    status
-                }
-            }
-        }
-    `,
-      });
-      const { entry } = data.data.entry;
-      setEntry(entry);
-    }
-  }, [route]);
+  //   const route = useRouter();
+  //   const [_entry, setEntry] = useState(entry);
+  //   useEffect(async () => {
+  //     const { query } = route;
+  //     const { token, slug } = query;
+  //     if (
+  //       !_entry &&
+  //       token &&
+  //       (query["x-craft-live-preview"] || query["x-craft-preview"])
+  //     ) {
+  //       // if craft preview send token
+  //       const headers = {
+  //         "x-Craft-Token": token ? `${token}` : "",
+  //       };
+  //       const { data } = await axios.post(process.env.API_URL, {
+  //         query: `
+  //             query BlogPost {
+  //             entry(slug: "${slug}", status: ["live", "disabled"]) {
+  //                 ...on blog_blog_Entry {
+  //                     title
+  //                     body
+  //                     status
+  //                 }
+  //             }
+  //         }
+  //     `,
+  //       });
+  //       const { entry } = data.data.entry;
+  //       setEntry(entry);
+  //     }
+  //   }, [route]);
   return (
     <>
-      {_entry ? (
+      {entry ? (
         <>
           <Head>
-            <title>{_entry.title}</title>
+            <title>{entry.title}</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
           <main className="flex flex-col max-w-6xl mx-auto">
             <div className="m-4">
-              <h1 className="text-lg font-bold">{_entry.title}</h1>
-              <div dangerouslySetInnerHTML={{ __html: _entry.body }} />
+              <h1 className="text-lg font-bold">{entry.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: entry.body }} />
             </div>
           </main>
         </>
@@ -73,7 +73,7 @@ export async function getStaticProps({ params, preview, previewData }) {
     {
       query: `
              query BlogPost {
-                entry(slug: "${slug}") {
+                entry(slug: "${slug}", status: ["live", "disabled"]) {
                     ...on blog_blog_Entry {
                         title
                         body
@@ -85,9 +85,13 @@ export async function getStaticProps({ params, preview, previewData }) {
     },
     { headers }
   );
+  console.log(headers, preview);
+  const { status } = data.data.entry;
+  const display =
+    status === "live" ? true : status === "disabled" && preview ? true : false;
   return {
     props: {
-      entry: data.data.entry,
+      entry: display ? data.data.entry : {},
     },
   };
 }
@@ -113,5 +117,5 @@ export async function getStaticPaths() {
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: true };
+  return { paths, fallback: false };
 }
